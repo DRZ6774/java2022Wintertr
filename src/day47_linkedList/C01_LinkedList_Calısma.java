4d65b35329ef66c0bdbfc486e4992c5c0ab70965
İdris Dıraz <diraz19056774@gmail.com>
2022-04-19 18:25:12 +0300

    19.04.2022

---
+++ src/day47_linkedList/C01_LinkedList_Calısma.java
@@ -0,0 +1,18 @@
+package day47_linkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class C01_LinkedList_Calısma {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> ll1 = new LinkedList<>();
+
+        ll1.add(10);
+        System.out.println(ll1); //[10]   add ekleme
+
+        // Sadece List interface'inden ozellikler kullanmak istersek
+        List<Integer> ll2 = new LinkedList<>();
+
+    }
+}
