d6764e6f4e95057035148cd1c5fcb1dcdf841210
İdris Dıraz <diraz19056774@gmail.com>
2022-04-26 11:25:51 +0300

    26.04.2022

--- src/day52_maps/C01_EntrySet_Calısma.java
+++ src/day52_maps/C01_EntrySet_Calısma.java
@@ -1,4 +1,34 @@
 package day52_maps;
 
+import day49_maps.MapOlustur;
+
+import java.util.Map;
+import java.util.Set;
+
 public class C01_EntrySet_Calısma {
+    public static void main(String[] args) {
+        Map<Integer,String> sinifListMap=MapOlustur.myMap();
+
+        System.out.println(sinifListMap);
+        //{101=Ali, Can, Dev, 1453, 102=Veli, Yan, QA, 1990, 103=Ali, Yan, C#, 2000, 104=Ayse, Kan, C#, 2001, 105=Fatma, Han, Java, 2002}
+
+        System.out.println(sinifListMap.entrySet());
+        //[101=Ali, Can, Dev, 1453, 102=Veli, Yan, QA, 1990, 103=Ali, Yan, C#, 2000, 104=Ayse, Kan, C#, 2001, 105=Fatma, Han, Java, 2002]
+
+        Set<Map.Entry<Integer,String>> sinifEntrySeti=sinifListMap.entrySet();
+        System.out.println("No   Isim    Soyisim   Brans  D.Yili");
+
+        for (Map.Entry<Integer,String>each:sinifEntrySeti
+             ) {
+            Integer entryKey=each.getKey();
+            String valueStr=each.getValue();
+
+            String valueArr[]=valueStr.split(",");
+
+            System.out.printf( "%-4d %-6s %-9s %-6s %s\n" , entryKey, valueArr[0], valueArr[1]
+                    , valueArr[2], valueArr[3]);
+            System.out.println("----------------------------------");
+        }
+
+    }
 }
--- src/day52_maps/C03_MapMethodlari_Calısma.java
+++ src/day52_maps/C03_MapMethodlari_Calısma.java
@@ -1,4 +1,34 @@
 package day52_maps;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class C03_MapMethodlari_Calısma {
+
+    public static void main(String[] args) {
+
+        Map<String,Integer> myMap= new HashMap<>();
+        myMap.put("H",3);
+        myMap.putIfAbsent("K",5);
+
+        System.out.println(myMap.putIfAbsent("A", 6));
+        if (myMap.putIfAbsent("K",20)!=null){
+            System.out.println("Girdiğiniz key map de zaten var ");
+        }
+        System.out.println(myMap);
+
+       myMap.put("A",10);
+        System.out.println(myMap); //{A=10, H=3, K=5}
+
+        myMap.computeIfAbsent("A", v->20);
+        System.out.println(myMap); //{A=10, H=3, K=5}
+
+        myMap.compute("A",(key,value)->20);
+        System.out.println(myMap); //{A=20, H=3, K=5}
+
+        // H'nin degerini 2 katinin 5 fazlasi yapalim
+        myMap.compute("H",(key,value)->(2*value+5));
+        System.out.println(myMap); //{A=20, H=11, K=5}
+
+    }
 }
